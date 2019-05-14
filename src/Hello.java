247936a36ef558223c643d089e7ab43d6999aeac
yeshimao <yeshimao@126.com>
2019-05-14 23:43:45 +0800

    第三次提交

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello World ");
         System.out.println("hehe");
+        System.out.println("haha");
     }
 }
